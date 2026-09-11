5d65f1339f9d799a8c0c5e3c6db6e5d307d3b55e
08es130-max <08es130@gmail.com>
2026-09-11 21:22:44 +0900

    Use repaired high-resolution default character icon

--- character-default-override.js
+++ character-default-override.js
@@ -4,7 +4,7 @@
   window.CHARACTER_LIBRARY.unshift({
     id: 'default',
     name: '三船栞子',
-    icon: 'icon-192-v030.png?v=0.3.0',
-    home: 'icon-192-v030.png?v=0.3.0'
+    icon: 'icon-512-v030.png?v=0.3.1',
+    home: 'icon-512-v030.png?v=0.3.1'
   });
 })();
