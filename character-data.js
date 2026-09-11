c5a3ff5efb529ca2da8a383d17a3f787bb22b1c6
08es130-max <08es130@gmail.com>
2026-09-11 15:06:36 +0900

    Add character library configuration

---
+++ character-data.js
@@ -0,0 +1,12 @@
+window.CHARACTER_LIBRARY = [
+  { id: 'default', name: 'デフォルト', icon: 'icon-192.png', home: 'icon-512.png' },
+  { id: 'char01', name: 'キャラ 1', icon: 'characters/char01.png' },
+  { id: 'char02', name: 'キャラ 2', icon: 'characters/char02.png' },
+  { id: 'char03', name: 'キャラ 3', icon: 'characters/char03.png' },
+  { id: 'char04', name: 'キャラ 4', icon: 'characters/char04.png' },
+  { id: 'char05', name: 'キャラ 5', icon: 'characters/char05.png' },
+  { id: 'char06', name: 'キャラ 6', icon: 'characters/char06.png' },
+  { id: 'char07', name: 'キャラ 7', icon: 'characters/char07.png' },
+  { id: 'char08', name: 'キャラ 8', icon: 'characters/char08.png' },
+  { id: 'char09', name: 'キャラ 9', icon: 'characters/char09.png' }
+];
